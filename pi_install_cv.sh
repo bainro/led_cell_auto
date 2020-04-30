f61672aa225efef60261504bbb094d83d00e4752
bainro <rbain18@gmail.com>
2020-04-30 10:53:38 -0700

    added opencv install bash script

---
+++ pi_install_cv.sh
@@ -0,0 +1,9 @@
+pip install opencv-python
+pip3 install opencv-contrib-python
+sudo apt install libjasper1
+sudo apt-get --yes --force-yes install libatlas-base-dev
+sudo apt-get update
+sudo apt-get install libhdf5-dev
+sudo apt-get install libhdf5-serial-dev
+sudo apt install libatlas3-base libsz2 libharfbuzz0b libtiff5 libjasper1 libilmbase12 libopenexr22 libilmbase12 libgstreamer1.0-0 libavcodec57 libavformat57 libavutil55 libswscale4 libqtgui4 libqt4-test libqtcore4
+
