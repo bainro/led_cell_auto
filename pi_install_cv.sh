5a88adf1387879096d0816e27ea243bfd0447881
Robert Bain <rbain18@gmail.com>
2020-04-30 11:05:32 -0700

    cv script changes. Should work for rpi4

--- pi_install_cv.sh
+++ pi_install_cv.sh
@@ -1,9 +1,10 @@
+sudo apt-get update
 pip install opencv-python
-pip3 install opencv-contrib-python
+pip install opencv-contrib-python==4.1.0.25
 sudo apt install libjasper1
 sudo apt-get --yes --force-yes install libatlas-base-dev
-sudo apt-get update
+sudo apt-get install -y libqtgui4
+sudo apt install libqt4-test
 sudo apt-get install libhdf5-dev
 sudo apt-get install libhdf5-serial-dev
 sudo apt install libatlas3-base libsz2 libharfbuzz0b libtiff5 libjasper1 libilmbase12 libopenexr22 libilmbase12 libgstreamer1.0-0 libavcodec57 libavformat57 libavutil55 libswscale4 libqtgui4 libqt4-test libqtcore4
-
